f44499435be4ef53f414ce36386a6ad8953d073d
Привалов Сергей <sprivalov@sitronics.com>
2022-07-05 10:45:39 +0300

    DevicePage

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,13 +1,69 @@
-
-import React from 'react';
-
+import React from "react";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
+import bigStar from "../assets/bigStar.png";
 const DevicePage = () => {
-
-    return (
-        <div>
-            DevicePage
-        </div>
-    );
+  const device = {
+    id: 5,
+    name: "12 pro",
+    price: 100000,
+    raiting: 0,
+    img: "ca459bff-043b-43e4-bb28-d00bae0c6f42.jpg",
+    info: [{'sds':'sda'},{'sds':'sds'}, ,{'asas':'asassds'}]
+  };
+  return (
+    <Container className="mt-3">
+      <Row>
+        <Col md={4}>
+          <Image width={300} height={300} src={"device.img"} />
+        </Col>
+        <Col md={4}>
+          <Row className="d-flex flex-column align-items-center">
+            <h2>{"device.name"}</h2>
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{
+                background: `url(${bigStar}) no-repeat center center`,
+                width: 240,
+                height: 240,
+                backgroundSize: "cover",
+                fontSize: 64,
+              }}
+            >
+              {"5"}
+            </div>
+          </Row>
+        </Col>
+        <Col md={4}>
+          <Card
+            className="d-flex flex-column align-items-center justify-content-around"
+            style={{
+              width: 300,
+              height: 300,
+              fontSize: 32,
+              border: "5px solid lightgray",
+            }}
+          >
+            <h3>От: {device.price} руб.</h3>
+            <Button variant={"outline-dark"}>Добавить в корзину</Button>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="d-flex flex-column m-3">
+        <h1>Характеристики</h1>
+        {device.info.map((info, index) => (
+          <Row
+            key={'info.id'}
+            style={{
+              background: index % 2 === 0 ? "lightgray" : "transparent",
+              padding: 10,
+            }}
+          >
+            {'info.title'}: {'info.description'}
+          </Row>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
 export default DevicePage;
